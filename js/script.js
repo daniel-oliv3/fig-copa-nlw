916c7fba925f02138d4fa3f4d2c89493d5ecd994
Daniel Oliveira <danielsapup3@gmail.com>
2022-10-26 12:46:45 -0300

    Update card troca explo 2

--- js/script.js
+++ js/script.js
@@ -2,13 +2,28 @@ let isIgnite = true;
 
 function changeCard(event) {
     const card = event.currentTarget;
-    
+    const bg = isIgnite
+        ? "explorer"
+        : "ignite"
+    isIgnite = !isIgnite;
+    card.style.backgroundImage = `url(./img/bg-${bg}.svg)`;
+}
 
+
+
+
+
+
+
+
+/*let isIgnite = true;
+
+function changeCard(event) {
+    const card = event.currentTarget;
     const backgroundImage = isIgnite ? "url(./img/bg-ignite.svg)" : "url(./img/bg-explorer.svg)";
     isIgnite = !isIgnite;
-
     card.style.backgroundImage = backgroundImage;
-}
+}*/
 
 
 
